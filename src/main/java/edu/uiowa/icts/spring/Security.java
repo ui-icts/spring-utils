7181750de1a00d967866dfcf0bd010efefa93adb
Lorentzen, Ryan R <ryan-lorentzen@uiowa.edu>
2014-12-18 09:30:47 -0600

    add isAuthenticated method

--- src/main/java/edu/uiowa/icts/spring/Security.java
+++ src/main/java/edu/uiowa/icts/spring/Security.java
@@ -2,6 +2,7 @@ package edu.uiowa.icts.spring;
 
 import java.util.Collection;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,7 +11,7 @@ public class Security {
 
 	public static boolean hasRole( String role ) {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		if( authentication != null ){
+		if ( authentication != null ) {
 			Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
 			for ( GrantedAuthority ga : authorities ) {
 				if ( ga.getAuthority().equals( role ) ) {
@@ -20,5 +21,10 @@ public class Security {
 		}
 		return false;
 	}
-	
+
+	public static boolean isAuthenticated() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		return authentication != null && ! ( authentication instanceof AnonymousAuthenticationToken ) && authentication.isAuthenticated();
+	}
+
 }
